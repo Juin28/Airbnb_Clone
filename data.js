8a730f72819baa505131aea8d4fadb84ec71a50d
JUIN TAN <tanjuinlong@gmail.com>
2024-06-24 11:37:12 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,142 @@
+export default [
+    {
+        id: 1,
+        title: "Make core memories with Inside Out 2",
+        host: "Joy",
+        // description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 50,
+        coverImg: "inside_out.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Wake up in the Musée d`Orsay",
+        host: "Mathieu Lehanneur",
+        // description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 300,
+        coverImg: "Musée_dOrsay.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Paris",
+        openSpots: 5,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        host: "David",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Go on tour with Feid",
+        host: "Feid",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 120,
+        coverImg: "feid.png",
+        stats: {
+            rating: 4.5,
+            reviewCount: 5
+        },
+        location: "United States",
+        openSpots: 10,
+    },
+    {
+        id: 5,
+        title: "Ted Lasso`s Favorite Pub",
+        host: "Mae",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 80,
+        coverImg: "pub.png",
+        stats: {
+            rating: 4.9,
+            reviewCount: 50
+        },
+        location: "United Kingdom",
+        openSpots: 40,
+    },
+    {
+        id: 6,
+        title: "Barbie`s Malibu DreamHouse, Ken`s Way",
+        host: "Ken",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 100,
+        coverImg: "barbie.png",
+        stats: {
+            rating: 4.6,
+            reviewCount: 4
+        },
+        location: "United States",
+        openSpots: 60,
+    },
+    {
+        id: 7,
+        title: "Go VIP with Kevin Hart",
+        host: "Kevin Hart",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 150,
+        coverImg: "kevin_hart.png",
+        stats: {
+            rating: 4.7,
+            reviewCount: 150
+        },
+        location: "United States",
+        openSpots: 200,
+    },
+    {
+        id: 8,
+        title: "Join a living room session with Doja",
+        host: "Doja Cat",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 100,
+        coverImg: "doja_cat.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 70
+        },
+        location: "United States",
+        openSpots: 40,
+    },
+    {
+        id: 9,
+        title: "Train at the X-Mansion",
+        host: "Jubilee",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 70,
+        coverImg: "x-mansion.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 50
+        },
+        location: "United States",
+        openSpots: 10,
+    },
+    {
+        id: 10,
+        title: "Design your Incredibles Supersuit",
+        host: "Edna Mode",
+        // description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 130,
+        coverImg: "supersuit.png",
+        stats: {
+            rating: 4.9,
+            reviewCount: 35
+        },
+        location: "United States",
+        openSpots: 15,
+    }
+]
